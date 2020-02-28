1e9f3889ebc643f5de2fb63c5e448b29a6e6d51f
kzoey <k_zoey94@naver.com>
2020-02-28 10:29:50 +0900

    햄이 커밋

--- src/main/java/com/todaylesson/service/MailSendService.java
+++ src/main/java/com/todaylesson/service/MailSendService.java
@@ -63,10 +63,10 @@ public class MailSendService {
 			String htmlStr = "<h2>안녕하세요 '"+ member_id +"' 님</h2><br><br>" 
 					+ "<p>비밀번호 찾기를 신청해주셔서 임시 비밀번호를 발급해드렸습니다.</p>"
 					+ "<p>임시로 발급 드린 비밀번호는 <h2 style='color : blue'>'" + key +"'</h2>이며 로그인 후 마이페이지에서 비밀번호를 변경해주시면 됩니다.</p><br>"
-					+ "<h3><a href='http://54.180.123.73/MS/'>MS :p 홈페이지 접속 ^0^</a></h3><br><br>"
+					+ "<h3><a href='http://54.180.123.73/MS/'>TodayLesson :p 홈페이지 접속 ^0^</a></h3><br><br>"
 					+ "(혹시 잘못 전달된 메일이라면 이 이메일을 무시하셔도 됩니다)";
 			try {
-				mail.setSubject("[MS :p] 임시 비밀번호가 발급되었습니다", "utf-8");
+				mail.setSubject("[TodayLesson] 임시 비밀번호가 발급되었습니다", "utf-8");
 				mail.setText(htmlStr, "utf-8", "html");
 				mail.addRecipient(RecipientType.TO, new InternetAddress(member_email));
 				mailSender.send(mail);
